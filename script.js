c374c080062e804d20aa59f73e16192add47bb87
lj-alexander-feinstein <ljalexander.803@gmail.com>
2023-07-29 14:22:27 -0700

    Added a functional timer that starts and stops.

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 let timer = document.getElementById("timer");
 const pomodoro = document.getElementById("pomodoro");
-const takeABreak = document.getElementById("break");
+const takeAShortBreak = document.getElementById("short-break");
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
+let minutes = 25;
+let time = minutes * 60;
+let interval = "";
+
 
 //element.addEventListener("click", function);
 
 pomodoro.addEventListener("click", pomodoroTimer);
-takeABreak.addEventListener("click", breakTimer);
+takeAShortBreak.addEventListener("click", shortBreakTimer);
 start.addEventListener("click", startControl)
+stop.addEventListener("click", reset)
 
 
 function pomodoroTimer() {
-    alert("pomodoro");
+    document.getElementById("timer").innerText = "25:00";
+    minutes = 25;
 };
 
-function breakTimer() {
-    alert("break");
+function shortBreakTimer() {
+    document.getElementById("timer").innerText = "5:00";
+    //time in seconds
+    minutes = 5;
 };
+
 
 function startControl() {
+    interval = setInterval(timerDisplay, 1000);
 }
 
+function timerDisplay() {
+    minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    timer.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
+
+function reset() {
+    clearInterval(interval);
+}
